bdd8cc40615cd33df1e6d12e83da43b1ccbda664
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-19 09:17:36 -0700

    Added function to populate the template with meetup results

--- src/js/app.js
+++ src/js/app.js
@@ -37,15 +37,51 @@ $(function(){
     navigator.geolocation.getCurrentPosition(success, error);
   }
 
+  //Sets the template information for each meetup
+  function setMeetupInfo(data){
+    var result = $('.meetup-template').clone();
+    
+    var title = result.find('.title a');
+    title.attr('href', data.link);
+    title.text(data.name);
+
+    var members = result.find('.members');
+    members.text("Members: " + data.members);
+
+    var organizer = result.find('.organizer');
+    organizer.next().text(data.organizer.name);
+
+    var about = result.find('.about');
+    about.html(data.description);
+
+    return result;
+  }
+
+  function getMeetupLoc(data){
+    return {
+      lat : data.lat,
+      lng : data.lon
+    };
+  }
+
+  function googleMarkerIt(location){
+    ///Set up marker 
+  }
+
+  //Uses the given category id to search for nearby meetups via ajax
   function getMeetups(id){
+    var meetupLoc, meetupDesc;
+    $('.meetup-results').html('');
+
     var opts = { 
       lon: userLoc.lng,
       lat: userLoc.lat,
       radius: 30,
+      key : '1164a772e5b1343e4b264e2f36e3e',
       category: id,
-      page: 15
+      page: 16
     };
-  
+
     $.ajax({
       url: "https://api.meetup.com/find/groups",
       data: opts,
@@ -53,19 +89,32 @@ $(function(){
       type: "GET",
     })
     .done(function(data){
-      console.log(data);
+      $.each(data.data, function(index, value){
+        //Set the template information
+        meetupDesc = setMeetupInfo(value);
+        //Set the location of the meetup
+        meetupLoc = getMeetupLoc(value);
+
+        //Attach results to the dom
+        $('.meetup-results').append(meetupDesc[0]);
+        //Attach marker to the map
+        googleMarkerIt(meetupLoc);
+      });
     })
     .fail(function(error){
-
+      errorMsg.html("Error trying to retrieve Meetups information.");
     });
   }
 
   getUserLocation();
 
+  //Sets submit listener for form
   $('.search-form').submit(function(e){
     e.preventDefault();
     var catId = $('.category').val();
+
     if(catId === null) {return;}
+
     //search for meetups with the given category
     getMeetups(catId);
   });
